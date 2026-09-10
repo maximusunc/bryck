851c285b6706ac014930422d3056315b80ead48b
Max Wang <max@covar.com>
2026-09-09 20:17:55 -0400

    Initial Commit

---
+++ Brick/BrickApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrickApp.swift
+//  Brick
+//
+//  Created by Max Wang on 9/9/26.
+//
+
+import SwiftUI
+
+@main
+struct BrickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
